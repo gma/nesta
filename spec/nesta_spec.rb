1b0a68ff52193ccf2106711d7cb0c8c0375adf3e
Graham Ashton <graham@effectif.com>
2008-11-27 16:41:24 +0000

    Refactored home page specs.

--- spec/nesta_spec.rb
+++ spec/nesta_spec.rb
@@ -6,11 +6,22 @@ describe "home page" do
     Nesta::Configuration.stubs(:configuration).returns({
       "blog" => { "title" => "My blog", "subheading" => "about stuff" }
     })
+    get_it "/"
+  end
+
+  it "should render successfully" do
+    should.be.ok
   end
   
-  it "should display site title" do
-    get_it "/"
-    should.be.ok
-    @response.body.should.match "My blog"
+  it "should display title in title tag" do
+    @response.body.should.match "<title>My blog</title>"
+  end
+  
+  it "should display site title in h1 tag" do
+    @response.body.should.match %r{<h1>\s*<a href='/'>My blog}
+  end
+  
+  it "should display site subheading in h1 tag" do
+    @response.body.should.match %r{about stuff\s*</small>\s*</h1}
   end
 end
