63c119881400eef854a0ef379cd83c6734186d23
Graham Ashton <graham@effectif.com>
2010-06-22 12:06:07 +0100

    Fixed caching bug; you could never enable it.

--- app.rb
+++ app.rb
@@ -10,7 +10,7 @@ require "lib/models"
 require "lib/path"
 require "lib/overrides"
 
-set :cache_enabled, (Nesta::Config.cache == "true")
+set :cache_enabled, Nesta::Config.cache
 
 helpers do
   def set_from_config(*variables)
