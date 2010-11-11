e41efd04fdc8d9bf5a56f48c8d0042d7f28c0e56
Graham Ashton <graham@effectif.com>
2010-11-11 23:40:58 +0000

    Improved clarity of rendering methods.

--- lib/overrides.rb
+++ lib/overrides.rb
@@ -2,28 +2,24 @@ module Nesta
   module Overrides
     module Renderers
       def haml(template, options = {}, locals = {})
-        render_options = Nesta::Overrides.render_options(template, :haml)
-        super(template, render_options.merge(options), locals)
+        defaults = Nesta::Overrides.render_options(template, :haml)
+        super(template, defaults.merge(options), locals)
       end
 
       def scss(template, options = {}, locals = {})
-        render_options = Nesta::Overrides.render_options(template, :scss)
-        super(template, render_options.merge(options), locals)
+        defaults = Nesta::Overrides.render_options(template, :scss)
+        super(template, defaults.merge(options), locals)
       end
 
       def sass(template, options = {}, locals = {})
-        render_options = Nesta::Overrides.render_options(template, :sass)
-        super(template, render_options.merge(options), locals)
+        defaults = Nesta::Overrides.render_options(template, :sass)
+        super(template, defaults.merge(options), locals)
       end
     end
 
     def self.load_local_app
       require Nesta::Path.local("app")
     rescue LoadError
-    end
-    
-    def self.local_view_path
-      Nesta::Path.local("views")
     end
     
     def self.load_theme_app
@@ -33,26 +29,31 @@ module Nesta
     rescue LoadError
     end
 
-    def self.theme_view_path
-      if Nesta::Config.theme.nil?
-        nil
-      else
-        File.join(Nesta::Path.themes, Nesta::Config.theme, "views")
+    private
+      def self.template_exists?(engine, views, template)
+        views && File.exist?(File.join(views, "#{template}.#{engine}"))
       end
-    end
 
-    def self.template_exists?(engine, views, template)
-      views && File.exist?(File.join(views, "#{template}.#{engine}"))
-    end
-
-    def self.render_options(template, engine)
-      if template_exists?(engine, local_view_path, template)
-        { :views => local_view_path }
-      elsif template_exists?(engine, theme_view_path, template)
-        { :views => theme_view_path }
-      else
-        {}
+      def self.render_options(template, engine)
+        if template_exists?(engine, local_view_path, template)
+          { :views => local_view_path }
+        elsif template_exists?(engine, theme_view_path, template)
+          { :views => theme_view_path }
+        else
+          {}
+        end
       end
-    end
+
+      def self.local_view_path
+        Nesta::Path.local("views")
+      end
+    
+      def self.theme_view_path
+        if Nesta::Config.theme.nil?
+          nil
+        else
+          Nesta::Path.themes(Nesta::Config.theme, "views")
+        end
+      end
   end
 end
