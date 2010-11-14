b2a0021351561b71642a0d3b180701fe95bbd186
Graham Ashton <graham@effectif.com>
2010-11-14 23:34:37 +0000

    Demonstrate how to use local/app.rb.

---
+++ local/app.rb
@@ -0,0 +1,12 @@
+module Nesta
+  class App
+    helpers do
+      # Define your own helper methods here.
+    end
+
+    # Define new actions (or override existing ones) here.
+    # get '/hello' do
+    #   'Hello!'
+    # end
+  end
+end
