5ab0e58c9ed93963ce59aca62c282ca478acb272
Graham Ashton <graham@effectif.com>
2010-11-15 00:46:50 +0000

    Use File.expand_path for 1.8 compatibility.

--- spec/path_spec.rb
+++ spec/path_spec.rb
@@ -2,7 +2,7 @@ require File.expand_path("spec_helper", File.dirname(__FILE__))
 
 describe "Nesta::Path" do
   before(:each) do
-    @root = File.absolute_path(File.dirname(File.dirname(__FILE__)))
+    @root = File.expand_path('..', File.dirname(__FILE__))
     @local_foo_bar = File.join(@root, "local", "foo/bar")
   end
 
