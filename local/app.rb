1dfab069b955f020fa5e390ca842f2959918e0d7
Carl Furrow <carl.furrow@gmail.com>
2010-11-24 21:26:04 -0500

    pulled out custom local/app changes

--- local/app.rb
+++ local/app.rb
@@ -1,27 +1,7 @@
 module Nesta
   class App
     helpers do
-      def gist(gn,filename=nil)
-        if(!filename.nil?)
-          filename = "?file=#{filename}"
-        end
-        "<script type='text/javascript' src='https://gist.github.com/#{gn}.js#{filename}'></script>"
-      end
-
-      def link_to(url,text=nil)
-          "<a href='#{url}'>#{text || url}</a>"
-      end
-    end
-
-    get "/" do
-      set_common_variables
-      set_from_config(:title, :subtitle, :description, :keywords)
-      @heading = @title
-      @title = "#{@title} - #{@subtitle}"
-      # paginate?
-      @articles = Page.find_articles[0..20]
-      @body_class = "home"
-      cache haml(:index)
+      # Define your own helper methods here.
     end
 
     # Define new actions (or override existing ones) here.
