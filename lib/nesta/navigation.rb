a635cd4b6f78e0282696393899de206a20e717a9
Graham Ashton <graham@effectif.com>
2010-12-21 14:58:12 +0000

    Split display_menu() into two methods.

--- lib/nesta/navigation.rb
+++ lib/nesta/navigation.rb
@@ -7,20 +7,23 @@ module Nesta
         if options[:levels] > 0
           haml_tag :ul, :class => options[:class] do
             menu.each do |item|
-              haml_tag :li do
-                if item.respond_to?(:each)
-                  display_menu(item, :levels => (options[:levels] - 1))
-                else
-                  haml_tag :a, :href => item.abspath do
-                    haml_concat item.heading
-                  end
-                end
-              end
+              display_menu_item(item, options)
             end
           end
         end
       end
 
+      def display_menu_item(item, options = {})
+        haml_tag :li do
+          if item.respond_to?(:each)
+            display_menu(item, :levels => (options[:levels] - 1))
+          else
+            haml_tag :a, :href => item.abspath do
+              haml_concat item.heading
+            end
+          end
+        end
+      end
     end
   end
 end
