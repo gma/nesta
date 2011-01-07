5012a509d40d7c8d279f8ed7a2ceea798d321f6f
Graham Ashton <graham@effectif.com>
2011-01-07 14:56:39 +0000

    Committed templates/themes (whoops).

---
+++ templates/themes/README.md
@@ -0,0 +1,7 @@
+<%= @name.capitalize.gsub(/[_-]/, ' ') %> Nesta theme
+<%= '=' * @name.length %>============
+
+<%= @name %> is a theme for Nesta, a [Ruby CMS](nesta), designed by
+<insert your name here>.
+
+[nesta]: http://effectif.com/nesta
---
+++ templates/themes/app.rb
@@ -0,0 +1,19 @@
+# Use the app.rb file to load Ruby code, modify or extend the models, or
+# do whatever else you fancy when the theme is loaded.
+
+module Nesta
+  class App
+    # Uncomment the Rack::Static line below if your theme has assets
+    # (i.e images or JavaScript).
+    #
+    # Put your assets in themes/<%= @name %>/public/<%= @name %>.
+    #
+    # use Rack::Static, :urls => ["/<%= @name %>"], :root => "themes/<%= @name %>/public"
+
+    helpers do
+      # Add new helpers here.
+    end
+
+    # Add new routes here.
+  end
+end
