276d7c32faab4bf2109f404a6a0e044923e0d92e
Graham Ashton <graham@effectif.com>
2011-01-17 15:31:41 +0000

    Implement the breadcrumb with a loop.

--- lib/nesta/navigation.rb
+++ lib/nesta/navigation.rb
@@ -24,6 +24,33 @@ module Nesta
           end
         end
       end
+
+      def breadcrumb_ancestors
+        ancestors = []
+        page = @page
+        while page
+          ancestors << page
+          page = page.parent
+        end
+        ancestors.reverse
+      end
+
+      def display_breadcrumbs(options = {})
+        haml_tag :ul, :class => options[:class] do
+          breadcrumb_ancestors[0...-1].each do |page|
+            haml_tag :li do
+              haml_tag :a, :href => page.abspath do
+                haml_concat breadcrumb_label(page)
+              end
+            end
+          end
+          haml_tag(:li) { haml_concat breadcrumb_label(@page) }
+        end
+      end
+
+      def breadcrumb_label(page)
+        (page.abspath == '/') ? 'Home' : page.heading
+      end
     end
   end
 end
