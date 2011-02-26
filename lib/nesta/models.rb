ed8ac5dacb89041f3d7080bbbd883da401e97a84
Christopher Lindblom <lindblom@dedge.se>
2011-02-26 18:33:58 +0100

    fixed so that title doesn't use parent.heading if parent.heading.nil?

--- lib/nesta/models.rb
+++ lib/nesta/models.rb
@@ -108,6 +108,10 @@ module Nesta
       metadata("keywords")
     end
     
+    def use_comments?
+      metadata("comments").nil? ? false : true
+    end
+    
     def metadata(key)
       @metadata[key]
     end
@@ -173,7 +177,7 @@ module Nesta
     def title
       if metadata('title')
         metadata('title')
-      elsif parent
+      elsif parent && !parent.heading.nil?
         "#{heading} - #{parent.heading}"
       elsif heading
         "#{heading} - #{Nesta::Config.title}"
