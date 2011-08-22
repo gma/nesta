33fe1bcee4c9f1cfd6ef1944bf1e88f22be6fffd
Graham Ashton <graham@effectif.com>
2011-08-22 23:14:54 +0100

    Set Nesta::App.root (Closes #59).

--- lib/nesta/path.rb
+++ lib/nesta/path.rb
@@ -1,11 +1,11 @@
 module Nesta
   class Path
     def self.local(*args)
-      File.expand_path(File.join(args), Nesta::Env.root)
+      File.expand_path(File.join(args), Nesta::App.root)
     end
 
     def self.themes(*args)
-      File.expand_path(File.join('themes', *args), Nesta::Env.root)
+      File.expand_path(File.join('themes', *args), Nesta::App.root)
     end
   end
 end
