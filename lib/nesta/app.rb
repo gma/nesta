954d3ff68225772535afff2a0353a0856571ded7
Brian Cantrell <bcantrell@gmail.com>
2012-08-26 18:14:57 -0400

    Adding @ to regex matching attachments. Otherwise Nesta returns not_found for high resolution images using the @2x convention a la retina.js

--- lib/nesta/app.rb
+++ lib/nesta/app.rb
@@ -59,11 +59,11 @@ module Nesta
       cache stylesheet(params[:sheet].to_sym)
     end
 
-    get %r{/attachments/([\w/.-]+)} do |file|
+    get %r{/attachments/([\w/.-@]+)} do |file|
       file = File.join(Nesta::Config.attachment_path, params[:captures].first)
       if file =~ /\.\.\//
         not_found
-      else 
+      else
         send_file(file, :disposition => nil)
       end
     end
