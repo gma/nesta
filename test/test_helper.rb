b56a0bd1bbf1d5bcb71cedfa523cacde7684299d
Graham Ashton <graham@effectif.com>
2016-07-28 20:29:53 +0100

    Format test output with minitest-reporters gem

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,12 @@
 require 'sinatra'
 require 'minitest/autorun'
 
+require 'minitest/reporters'
+
+reporter_setting = ENV.fetch('REPORTER', 'progress')
+camel_case = reporter_setting.split(/_/).map { |word| word.capitalize }.join('')
+Minitest::Reporters.use! Minitest::Reporters.const_get("#{camel_case}Reporter").new
+
 require File.expand_path('../lib/nesta/env', File.dirname(__FILE__))
 require File.expand_path('../lib/nesta/app', File.dirname(__FILE__))
 
