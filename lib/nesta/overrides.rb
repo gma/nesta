19ec573c06a9443091919e9142ded96e92fbf76c
Graham Ashton <graham@effectif.com>
2023-03-11 14:01:34 +0000

    Replace custom code with Sinatra's #find_template
    Nesta allows you to put your template (e.g. Haml, Erb, Sass and CSS)
    files in a local `./views` folder, or in a folder inside a theme. It
    also supports loading default files from within the gem itself.
    
    Sinatra didn't originally support multiple template directories, so
    Nesta contained custom functionality to make it work. The overridable
    `#find_template` method was added to Sinatra, which makes it easier to
    do. [1]
    
    This was pointed out by @lilith back in 2012. It always looked sensible,
    but it wasn't pressing. But I'm working on upgrading Nesta to Sinatra 3,
    which has dropped support for Sass, so I need to refactor this code
    anyway. So let's start that change from a better place. Thanks @lilith!
    
    [1] http://www.sinatrarb.com/2011/03/03/sinatra-1.2.0.html

--- lib/nesta/overrides.rb
+++ lib/nesta/overrides.rb
@@ -1,30 +1,32 @@
 module Nesta
   module Overrides
     module Renderers
-      def haml(template, options = {}, locals = {})
-        defaults, engine = Overrides.render_options(template, :haml)
-        super(template, defaults.merge(options), locals)
-      end
-
-      def erb(template, options = {}, locals = {})
-        defaults, engine = Overrides.render_options(template, :erb)
-        super(template, defaults.merge(options), locals)
-      end
-
-      def scss(template, options = {}, locals = {})
-        defaults, engine = Overrides.render_options(template, :scss)
-        super(template, defaults.merge(options), locals)
-      end
-
-      def sass(template, options = {}, locals = {})
-        defaults, engine = Overrides.render_options(template, :sass)
-        super(template, defaults.merge(options), locals)
+      def find_template(views, name, engine, &block)
+        user_paths = [
+          Nesta::Overrides.local_view_path,
+          Nesta::Overrides.theme_view_path
+        ].compact
+        user_paths.append(views).flatten.each do |path|
+          super(path, name, engine, &block)
+        end
       end
 
       def stylesheet(template, options = {}, locals = {})
-        defaults, engine = Overrides.render_options(template, :sass, :scss)
-        renderer = Sinatra::Templates.instance_method(engine)
-        renderer.bind(self).call(template, defaults.merge(options), locals)
+        scss(template, options, locals)
+      rescue Errno::ENOENT
+        sass(template, options, locals)
+      end
+    end
+
+    def self.local_view_path
+      Nesta::Path.local("views")
+    end
+  
+    def self.theme_view_path
+      if Nesta::Config.theme.nil?
+        nil
+      else
+        Nesta::Path.themes(Nesta::Config.theme, "views")
       end
     end
 
@@ -39,33 +41,5 @@ module Nesta
         require app_file if File.exist?(app_file)
       end
     end
-
-    private
-      def self.template_exists?(engine, views, template)
-        views && File.exist?(File.join(views, "#{template}.#{engine}"))
-      end
-
-      def self.render_options(template, *engines)
-        [local_view_path, theme_view_path].each do |path|
-          engines.each do |engine|
-            if template_exists?(engine, path, template)
-              return { views: path }, engine
-            end
-          end
-        end
-        [{}, :sass]
-      end
-
-      def self.local_view_path
-        Nesta::Path.local("views")
-      end
-    
-      def self.theme_view_path
-        if Nesta::Config.theme.nil?
-          nil
-        else
-          Nesta::Path.themes(Nesta::Config.theme, "views")
-        end
-      end
   end
 end
